cd19b66802ac7c52035551ea869abcb21ddb2c89
Legatus12 <pablo.clase.daw@gmail.com>
2023-05-05 16:37:05 +0200

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,36 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 import Dashboard from './components/Dashboard'
 import { useState } from 'react'
+import useMountEffect from '@restart/hooks/useMountEffect'
+import { useTranslation } from 'react-i18next'
 
 const App = () => {
+
+  const { i18n } = useTranslation()
+
+  useMountEffect(() => {
+
+    let lan = localStorage.getItem('language')
+    console.log(lan)
+
+    if(lan == null) {
+      console.log('echo')
+      if(navigator.language.substring(0, 2) == 'es') {
+        lan = { key: 'es', name: 'Español' }
+        localStorage.setItem('language', lan)
+      } else {
+        lan = { key: 'es', name: 'English' }
+        localStorage.setItem('language', lan)
+        console.log(localStorage.getItem('language'))
+      }
+    } 
+    i18n.changeLanguage(lan.key)
+    
+  })
   
   const [user, setUser] = useState(null);
 
-  console.log(user)
+  //console.log(user)
   if(user === null){
     return (
       <BrowserRouter>
--- src/components/Dashboard/Settings.jsx
+++ src/components/Dashboard/Settings.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const Settings = () => {
 
-    const { t } = useTranslation()
+    const { t, i18n } = useTranslation()
+
+    const switchLanguage = (lan) => {
+      i18n.changeLanguage(lan)
+      localStorage.setItem('language', lan)
+    }
 
     return (
       <div className="settings full">
         <h1 className='settings-title'>{t('settings.title')}</h1>
+
         <h2>{t('settings.preferences')}</h2>
         <div className='settings-option'>
           <p>{t('settings.theme')}</p>
@@ -19,19 +26,25 @@ const Settings = () => {
         </div>
         <div className='settings-option'>
           <p>{t('settings.language')}</p>
-          <div class="flex items-center">
-            <select name="" id="">
-              <option value="">Español</option>
-              <option value="">English</option>
-            </select>
-          </div> 
+          <div className='auth-languages'>
+            <div className='flex justify-around items-center'>
+              <button onClick={() => switchLanguage('en')} className={ i18n.resolvedLanguage == "en" ? 'font-black w-4' : 'w-4' }>en</button>
+              <img src="src/img/en.png" className='w-8' />
+            </div>
+            <span className='w-[0.12rem] h-10 bg-silver'>&nbsp;</span>
+            <div className='flex justify-around items-center'>
+              <button onClick={() => switchLanguage('es')} className={ i18n.resolvedLanguage == "es" ? 'font-black w-4' : 'w-4' }>es</button>
+              <img src="src/img/es.png" className='w-8' />
+            </div>
+          </div>
         </div>
-        <hr />
+        
         <h2>{t('settings.notifications')}</h2>
-        <hr />
+
         <h2>{t('settings.language')}</h2>
-        <hr />
+
         <h2>{t('settings.account')}</h2>
+        
       </div>
     )
 }
