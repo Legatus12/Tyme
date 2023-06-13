ff436042a8e74fcae0022a0051597ab1a677c0e0
Lantigua Santos <plantigua@sia.es>
2023-06-13 21:03:43 +0200

    charts

--- src/components/Dashboard/Overview/Charts.jsx
+++ src/components/Dashboard/Overview/Charts.jsx
@@ -1,10 +1,51 @@
 import { useTranslation } from "react-i18next"
 import { Link } from "react-router-dom"
-import Chart from 'chart.js/auto'
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../../../AuthProvider"
+import { TymesByProjectChart } from "./Charts/TymesByProjectChart"
+import { getProjects, getTymes } from "../../../../firebase"
 
 const Charts = () => {
 
+  const user = useContext(AuthContext)
+
   const { t } = useTranslation()
+
+  const [projects, setProjects] = useState([])
+  const [tymes, setTymes] = useState([])
+  const [tymesByProject, setTymesByProject] = useState({})
+  
+  //
+
+  useEffect(() => {
+
+    getTymes(user.uid, (docs) => {
+      const arr = []
+      docs.forEach(doc => {
+        const aux = { id: doc.id, ...doc.data() }
+        arr.push(aux)
+      })
+      setProjects(arr)
+    })
+
+    getTymes(user.uid, (docs) => {
+      const arr = []
+      docs.forEach(doc => {
+        const aux = { id: doc.id, ...doc.data() }
+        arr.push(aux)
+      })
+      setTymes(arr)
+    })
+
+    setTymesByProject(tymes.reduce((acc, tyme) => {
+      const { project } = tyme
+      acc[project] = (acc[project] || 0) + 1
+      return acc
+    }, {}))
+    console.log(tymesByProject)
+  }, [user])
+
+  //
 
   return (
     <div className="charts full">
@@ -15,9 +56,7 @@ const Charts = () => {
         <h1>{t('charts.title')}</h1>
       </div>
       <div className="charts-container">
-        <div>
-          
-        </div>
+        <TymesByProjectChart tymesByProject={tymesByProject} />
       </div>
     </div>
   )
